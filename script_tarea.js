7db076076cb3a712f995c44ae9c56ad33c5eb854
ATM22 <149100086+ATM22@users.noreply.github.com>
2023-10-26 21:00:09 -0600

    Update script_tarea.js

--- script_tarea.js
+++ script_tarea.js
@@ -2,6 +2,7 @@ let empleados = [];
 let sumaSalarios = 0;
 let x =0;
 let sumaHoras=0;
+let prom=0;
 document.getElementById("tabla-empleados").style.display = "none";
 		function guardarEmpleado() {
 			let empleado = {};
@@ -16,64 +17,58 @@ document.getElementById("tabla-empleados").style.display = "none";
                         document.getElementById("dpi").value = "";
 			document.getElementById("salario").value = "";
 			document.getElementById("horas").value = "";
+                        let tabla = document.getElementById("tabla-empleados");
+tabla.innerHTML = "<tr><th>Empleado</th><th>Nombre</th><th>Edad</th><th>DPI</th><th>Salario mensual</th><th>Salario total</th><th>Horas extras</th><th>Boleto de ornato</th></tr>";
+    sumaHoras = 0;
+    sumaSalarios = 0;
+    for (let i = 0; i < empleados.length; i++) {
+        calcularBoletodeOrnato(i);
+        let total = parseFloat (empleados[i].salario - (empleados[i].salario * 0.0483) + 250);
+        let row = tabla.insertRow(-1);
+        let empleadoCell = row.insertCell(0);
+        let nombreCell = row.insertCell(1);
+        let edadCell = row.insertCell(2);
+        let dpiCell = row.insertCell(3);
+        let salarioCell = row.insertCell(4);
+        let totalCell = row.insertCell(5);
+        let horasCell = row.insertCell(6);
+        let boletoCell = row.insertCell(7);
+        empleadoCell.innerHTML = "Empleado " + (i + 1);
+        nombreCell.innerHTML = empleados[i].nombre;
+        edadCell.innerHTML = empleados[i].edad;
+        dpiCell.innerHTML = empleados[i].dpi;
+        salarioCell.innerHTML = "Q" + empleados[i].salario;
+        totalCell.innerHTML = "Q" + total;
+        horasCell.innerHTML = empleados[i].horas;
+        boletoCell.innerHTML = empleados[i].boleto ? empleados[i].boleto : "";
+        sumaSalarios += parseFloat(total);
+        sumaHoras += parseInt(empleados[i].horas);
+        x += 1;
+    }
+    document.getElementById("tabla-empleados").style.display = "block";
+    prom = parseFloat(sumaSalarios / x);
 		}
                 
-                function calcularBoletodeOrnato () {
-			guardarEmpleado();
-			for (let i = 0; i < empleados.length; i++) {
-				empleados[i].salario = parseFloat(empleados[i].salario;
-                                if (empleados[i].salario <= 500.00) {
-                                    empleados[i].boleto = "Monto de Q4.00. Con multa Q8.00 ";
-                                } else if (empleados[i].salario >= 500.01 && empleados[i].salario <= 1000.00) {
-                                    empleados[i].boleto = "Monto de Q10.00. Con multa Q20.00 ";
-                                } else if (empleados[i].salario >= 1000.01 && empleados[i].salario < 3000.00) {
-                                    empleados[i].boleto = "Monto de Q15.00. Con multa Q30.00";
-                                } else if (empleados[i].salario >= 3000.01 && empleados[i].salario < 6000.00) {
-                                    empleados[i].boleto = "Monto de Q50.00. Con multa Q100.00 ";
-                                } else if (empleados[i].salario >= 6000.01 && empleados[i].salario < 9000.00) {
-                                    empleados[i].boleto = "Monto de Q75.00. Con multa Q150.00 ";
-                                } else if (empleados[i].salario >= 9000.01 && empleados[i].salario < 12000.00) {
-                                    empleados[i].boleto = "Monto de Q100.00. Con multa Q200.00 ";
-                                } else if (empleados[i].salario >= 12000.01) {
-                                    empleados[i].boleto = "Monto de Q150.00. Con multa Q300.00 ";
-                                }
-			}
-                }
-    
+               function calcularBoletodeOrnato(index) {
+    let empleado = empleados[index];
+    if (empleado.salario <= 500.00) {
+        empleado.boleto = "Monto de Q4.00. Con multa Q8.00 ";
+    } else if (empleado.salario >= 500.01 && empleado.salario <= 1000.00) {
+        empleado.boleto = "Monto de Q10.00. Con multa Q20.00 ";
+    } else if (empleado.salario >= 1000.01 && empleado.salario <= 3000.00) {
+        empleado.boleto = "Monto de Q15.00. Con multa Q30.00";
+    } else if (empleado.salario >= 3000.01 && empleado.salario <= 6000.00) {
+        empleado.boleto = "Monto de Q50.00. Con multa Q100.00 ";
+    } else if (empleado.salario >= 6000.01 && empleado.salario <= 9000.00) {
+        empleado.boleto = "Monto de Q75.00. Con multa Q150.00 ";
+    } else if (empleado.salario >= 9000.01 && empleado.salario <= 12000.00) {
+        empleado.boleto = "Monto de Q100.00. Con multa Q200.00 ";
+    } else if (empleado.salario >= 12000.01) {
+        empleado.boleto = "Monto de Q150.00. Con multa Q300.00 ";
+    }
+}
 
-		function mostrarEmpleado() {
-                       let tabla = document.getElementById("tabla-empleados");
-tabla.innerHTML = "<tr><th>Empleado</th><th>Nombre</th><th>Edad</th><th>DPI</th><th>Salario mensual</th><th>Salario total</th><th>Horas extras</th><th>Boleto de ornato</th></tr>";
-  sumaHoras = 0;
-  sumaSalarios = 0;
-  for (let i = 0; i < empleados.length; i++) {
-    let total = parseFloat (empleados[i].salario - (empleados[i].salario * 0.0483) + 250);
-    let row = tabla.insertRow(-1);
-    let empleadoCell = row.insertCell(0);
-    let nombreCell = row.insertCell(1);
-    let edadCell = row.insertCell(2);
-    let dpiCell = row.insertCell(3);
-    let salarioCell = row.insertCell(4);
-    let totalCell = row.insertCell(5);
-    let horasCell = row.insertCell(6);
-    let boletoCell = row.insertCell(7);
-    empleadoCell.innerHTML = "Empleado " + (i + 1);
-    nombreCell.innerHTML = empleados[i].nombre;
-    edadCell.innerHTML = empleados[i].edad;
-    dpiCell.innerHTML = empleados[i].dpi;
-    salarioCell.innerHTML = "Q" + empleados[i].salario;
-    totalCell.innerHTML = "Q" + total;
-    horasCell.innerHTML = empleados[i].horas;
-    boletoCell.innerHTML = empleados[i].boleto;
-    sumaSalarios += parseFloat(total);
-    sumaHoras += parseInt(empleados[i].horas);
-    x += 1;
-  }
-  document.getElementById("tabla-empleados").style.display = "block";
-  let prom = parseFloat(sumaSalarios / x);
-  }
   function totales(){
-  mostrarEmpleado()
   let totales = "La suma de los salarios totales es de: Q" + sumaSalarios + "\n" +
     "EL promedio de salarios totales es de: Q" + prom + "\n" +
     "La suma de horas extras es: " + sumaHoras;
